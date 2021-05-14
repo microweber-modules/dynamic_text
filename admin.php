280d80e63f0f1d66c4cd1abd40722b30ff432182
Nikolay <rachkuw@gmail.com>
2021-05-14 16:03:47 +0300

    Add files via upload

--- admin.php
+++ admin.php
@@ -6,18 +6,9 @@
         <module type="admin/modules/info"/>
     <?php endif; ?>
     <div class="admin-side-content" style="max-width:100%;">
-
         <?php if (isset($params['live_edit'])): ?>
         <module type="dynamic_text/select" />
         <?php endif; ?>
-
         <module type="dynamic_text/edit" />
-
-        <br />
-        <br />
-
-        <module type="dynamic_text/list" />
-
-
     </div>
 </div>
