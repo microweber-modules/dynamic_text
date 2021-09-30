77fab726b6bb2e28804c15bd32fa108569bd2423
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-09-30 15:30:45 +0300

    update

--- edit.php
+++ edit.php
@@ -80,7 +80,7 @@ if (isset($params["live_edit"]) and $params["live_edit"]) {
                 <small class="text-muted d-block mb-3"><?php _e("Add new dynamic text then drop it in live edit."); ?></small>
                 <form id="save-dynamic-text-form" class="form-group js-toggle-form" style="display: none">
                     <div class="col-4">
-                        <label><?php _e("Variable"); ?>:</label>
+                        <label><?php _e("Variable key"); ?>:</label>
                         <small class="text-muted d-block mb-3"><?php _e("Example: 'my-cool-name'"); ?></small>
 
                         <input type="text" name="name" class="form-control js-dynamic-text-name" required="required">
